e28e85d7ecbfeca3db257b5097c350b8281e173f
Jacob Dymond <thedevjake@gmail.com>
2019-02-13 18:58:06 -0600

    Publish
    To-Do list built on ethereum

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-  uint public taskCount = 0;
+ uint public taskCount = 0;
 
   struct Task {
     uint id;
@@ -12,19 +12,31 @@ contract TodoList {
   mapping(uint => Task) public tasks;
 
   event TaskCreated(
-    uint id, 
+    uint id,
     string content,
     bool completed
   );
 
-  constructor () public {
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+  constructor() public {
     createTask("Check out thedevjake");
   }
 
   function createTask(string memory _content) public {
     taskCount ++;
     tasks[taskCount] = Task(taskCount, _content, false);
-    emit  TaskCreated(taskCount, _content, false);
-  } 
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
 
 }
